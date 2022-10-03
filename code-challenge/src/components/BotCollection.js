d24a0e625c524227c956f0c5b4a2498303e20d7c
AngieMwaniki <angela.mwaniki@student.moringaschool.com>
2022-10-03 17:10:21 +0300

    completed

---
+++ code-challenge/src/components/BotCollection.js
@@ -0,0 +1,20 @@
+import React from "react";
+import BotCard from "./BotCard";
+
+function BotCollection({bots,deleteBot,addToMyBotArmy}) {
+
+const botsList = bots.map((bot)=>{
+return (
+<BotCard addToMyBotArmy={addToMyBotArmy} deleteBot={deleteBot} key={bot.id} bot={bot}></BotCard>
+)
+})
+return (
+<div className="ui four column grid"> 
+<div className="row">
+{botsList}
+</div> 
+</div>
+);
+}
+
+export default BotCollection;
---
+++ code-challenge/src/components/BotsPage.js
@@ -0,0 +1,41 @@
+import React,{useEffect,useState} from "react";
+import YourBotArmy from "./YourBotArmy";
+import BotCollection from "./BotCollection";
+
+function BotsPage() {
+const [bots,setBots] = useState([]) 
+const [selectedBots, setSelectedBots] = useState([])
+
+useEffect(()=>{
+fetch("http://localhost:8002/bots")
+.then((response)=>response.json())
+.then((data)=>setBots(data))
+},[])
+function deletebot(bot){
+const filterbots = bots.filter(
+(singlebot) => singlebot.id !== bot.id
+);
+const deleteConfig = {
+method: "DELETE",
+headers: {
+"Content-Type": "application/json",
+} 
+}
+fetch(`http://localhost:8002/bots/${bot.id}`,deleteConfig)
+.then(()=>setBots(filterbots))
+}
+function addToMyBotArmy(bot){
+setSelectedBots((prevbot)=>{
+return [...prevbot,bot]
+})
+}
+console.log(addToMyBotArmy)
+return (
+<div>
+<YourBotArmy bots={selectedBots}/>
+<BotCollection addToMyBotArmy={addToMyBotArmy} deleteBot={deletebot} bots={bots} />
+</div>
+)
+}
+
+export default BotsPage;
